34ddef500253970603aa73169d7a831bd0b1a969
JuliaVaca <thesimpatic@hotmail.com>
2018-11-09 13:12:10 -0600

    Ejercicio4

---
+++ README.md
@@ -0,0 +1,8 @@
+# basic_list
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+ runApp(new MaterialApp(home: new Application()));
+}
+class Application extends StatefulWidget {
+ @override
+ _ApplicationState createState() => new _ApplicationState();
+}
+class _ApplicationState extends State<Application> {
+Widget build(BuildContext context) {
+ return new Scaffold(
+ body: new ListView(
+ children: <Widget>[
+ new ListTile(
+ title: new Text('This is Title'),
+ isThreeLine: true,
+ subtitle: new Text('This is our Subtitle'),
+ trailing: new Icon(Icons.close),
+ ),
+ new ListTile(
+ title: new Text('This is Title'),
+ isThreeLine: true,
+ subtitle: new Text('This is our Subtitle'),
+ trailing: new Icon(Icons.close),
+ ),
+ new ListTile(
+ title: new Text('This is Title'),
+ isThreeLine: true,
+ subtitle: new Text('This is our Subtitle'),
+ trailing: new Icon(Icons.close),
+ ),
+ ],
+ )
+ );
+ }
+}
